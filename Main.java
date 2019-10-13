ae37e7ce2e4ff574354ded94e7348f2b57dfb42e
mark <markhinojosa1212@gmail.com>
2019-10-12 23:56:49 -0500

    added false case handling

--- Main.java
+++ Main.java
@@ -3,17 +3,39 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+
+        int principalValue = 0;
+        float AIR = 0;
+        int termLengthInYears;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principalValue = scanner.nextInt();
-        System.out.print("Your Annual Interest Rate : " );
-        float AIR = scanner.nextFloat();
-        System.out.print("Term length in years: ");
-        int period = scanner.nextInt();
-        int periodInMonths = period * 12;
+        while (true){
+            System.out.print("Principal (1k - 1M): ");
+            principalValue = scanner.nextInt();
+            if(principalValue >= 1000 && principalValue <= 100_000_000)
+                break;
+            System.out.println("Enter a Value between 1000 and 100,000,000");
+
+        }
+        while (true){
+            System.out.print("Your Annual Interest Rate : " );
+            AIR = scanner.nextFloat();
+            if(AIR > 0 && AIR < 25)
+                break;
+            System.out.println("Enter a value greater than 0 and less than 25");
+        }
+        while (true){
+            System.out.print("Term length in years: ");
+            termLengthInYears = scanner.nextInt();
+            if(termLengthInYears >= 1 && termLengthInYears <= 30 )
+                break;
+            System.out.println("Enter a term length between 0 and 30 years");
+        }
+
+        int termLengthInMonths = termLengthInYears * 12;
         float monthlyInterestRate = AIR / 100 / 12;
-        double topMath = monthlyInterestRate *  Math.pow((1 + monthlyInterestRate), periodInMonths);
-        double bottomMath =  Math.pow(( 1 + monthlyInterestRate ), periodInMonths) - 1;
+        double topMath = monthlyInterestRate *  Math.pow((1 + monthlyInterestRate), termLengthInMonths);
+        double bottomMath =  Math.pow(( 1 + monthlyInterestRate ), termLengthInMonths) - 1;
         double mortgage = principalValue
                 * topMath
                 / bottomMath;
